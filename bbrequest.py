b6bb4993945b82e7eff8d63a3c405c635507e999
TDNiki <Nikid-z@yandex.ru>
2024-10-10 13:40:23 +0500

    Added an annotation

--- bbrequest.py
+++ bbrequest.py
@@ -10,10 +10,10 @@ class ParsingLinks_Error(bb_connect_Error):
         super().__init__(msg, *args)
 
 class bb_connect:
-    """Получения, распаковка пакетов расписания"""
+    """Gets all url .xls from url directory"""
 
     __EXT_EXCEL = '.xls'
-    __FIND_PARAM = '[id$="xythosFileAnchor"]'
+    __FIND_PARAM = '[id$="xythosFileAnchor"]' 
     
     def __init__(self, bb_link: str) -> None:
         if type(bb_link) is not str: raise TypeError('Param bb_link must be str type')
@@ -22,7 +22,6 @@ class bb_connect:
     
     def get_urls(self) -> list:
         """Gets all url .xls from url directory"""
-        #It is possible to add a function, so there is a redirection for now
         return self.__to_xld_files()
     
     def __to_xld_files(self) -> list:
@@ -35,16 +34,17 @@ class bb_connect:
 
         while len(elements):
             try:
-                link = elements[0].absolute_links.pop()
+                link = elements[0].absolute_links.pop() #absolute_links: set; must be only one link in set
                 if link.endswith(self.__EXT_EXCEL):
                     urls.append(link)
                 else:
-                    elements.extend(ses.get(link).html.find(self.__FIND_PARAM))
+                    elements.extend(ses.get(link).html.find(self.__FIND_PARAM)) #merge lists
                 elements.pop(0)
 
             except KeyError:
-                pass
+                pass #dir doesn't has any folders, files
             except Exception as err:
                 raise ParsingLinks_Error(err)
 
         return urls
+        
